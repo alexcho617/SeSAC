3119ad71e312d1eb9b18ee7d05a6defe88691429
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-25 11:46:28 +0900

    Manually Uploaded Week2 proj

---
+++ 2week/music/music/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  music
+//
+//  Created by Alex Cho on 2023/07/24.
+//
+
+import UIKit
+import IQKeyboardManagerSwift //step1
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true //step2
+
+        return true
+    }
+
+    
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
+
+
+}
+
