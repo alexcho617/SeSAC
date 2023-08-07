661d0746ae0777b1fd453d06ec5f899b38900b00
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-08-07 21:39:02 +0900

    end of 8/7 class

--- week4/week4/week4/ViewController.swift
+++ week4/week4/week4/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callRequest()
+    }
+    
+    func callRequest(){
+        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.boxOfficeKey)&targetDt=20120101"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                print("======")
+                let name = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"]
+                print(name)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
