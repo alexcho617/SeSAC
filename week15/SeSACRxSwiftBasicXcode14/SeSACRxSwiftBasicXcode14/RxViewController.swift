82841fa3408bab8226efae386192aa5d90b560c7
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-10-31 23:40:17 +0900

    end of 10/31 class: rxswift

--- week15/SeSACRxSwiftBasicXcode14/SeSACRxSwiftBasicXcode14/RxViewController.swift
+++ week15/SeSACRxSwiftBasicXcode14/SeSACRxSwiftBasicXcode14/RxViewController.swift
@@ -13,33 +13,34 @@ class RxViewController: UIViewController {
     
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var changeButton: UIButton!
-//    var nickname = Observable.just("고래밥") //just 연산자
+    //    var nickname = Observable.just("고래밥") //just 연산자
     var nickname = BehaviorSubject(value: "고래밥")
-//    var nickname = BehaviorRelay(value: "고래밥")
+    //    var nickname = BehaviorRelay(value: "고래밥")
     @IBOutlet var timerLabel: UILabel!
     
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sample()
         
         nickname
             .bind(to: nameLabel.rx.text) //가변 매개변수: 여러개 넣을 수 있음
             .disposed(by: disposeBag)
         
-//        //observable
-//        nickname.subscribe { value in //next
-//            print(value)
-//            self.nameLabel.text = value
-//        } onError: { error in
-//            print(error)
-//        } onCompleted: {
-//            print("nickname - onCompleted")
-//        } onDisposed: {
-//            print("nickname = onDisposed")
-//        }
-//        .dispose()
-
+        //        //observable
+        //        nickname.subscribe { value in //next
+        //            print(value)
+        //            self.nameLabel.text = value
+        //        } onError: { error in
+        //            print(error)
+        //        } onCompleted: {
+        //            print("nickname - onCompleted")
+        //        } onDisposed: {
+        //            print("nickname = onDisposed")
+        //        }
+        //        .dispose()
+        
         changeButton.rx.tap
             .subscribe { value in
                 print("버튼 클릭 \(value)")
@@ -71,5 +72,28 @@ class RxViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             self.disposeBag = DisposeBag()
         }
+    }
+    
+    func sample(){
+        let item = [1,2,3,4,5]
+        let item2 = [5,4,3,2,1]
+        //just는 그냥 보냄,
+//        Observable.just(item) 1개 왜 구분했나? of 하나만 있으면 되는거 아닌가
+//        Observable.of(item, item2) //여러개, onNext 여러번
+//        Observable.from(item) //array element 하나씩 전달 (for)
+        Observable.repeatElement(1) //무한반복
+            .take(5) //n번 반복
+            .subscribe { val in
+                print("Subscribe - \(val)")
+            } onError: { error in
+                print("Error - \(error)")
+            } onCompleted: {
+                print("onCompleted")
+                //dispose
+            } onDisposed: {
+                print("onDisposed")
+                //dispose
+            }.disposed(by: disposeBag)
+
     }
 }
