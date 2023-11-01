b98d1b5d42b77faf4fccdfa8bf168ef7c65dc52d
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-11-02 01:09:16 +0900

    phone vc assignment

--- week16/assignment/SeSACRxThreads/SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ week16/assignment/SeSACRxThreads/SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,19 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    let phoneNumber = BehaviorSubject(value: "010")
+//    let phone = BehaviorSubject(value: "010")
+    let buttonColor = BehaviorSubject(value: UIColor.red)
+    let buttonEnabled = BehaviorSubject(value: false)
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +29,40 @@ class PhoneViewController: UIViewController {
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
+    }
+    
+    func bind(){
+        //buttonEnabled <->button
+        buttonEnabled.bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        //buttonColor <->button background, textfield color
+        buttonColor.bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
+            .disposed(by: disposeBag)
+
+        //buttonColor <-> textfield border
+        buttonColor.map {$0.cgColor}
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+
+        //number <-> textfield text
+        phoneNumber.bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        //number -> bool <->button enabled, buttonColor
+        phoneNumber.map {$0.count == 13}
+            .subscribe(with: self) { owner, value in
+                owner.buttonEnabled.onNext(value)
+                owner.buttonColor.onNext(value ? UIColor.black : UIColor.red)
+            }.disposed(by: disposeBag)
+
+        //textfield text <-> number
+        phoneTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, value in
+                let result = value.formated(by: "###-####-####")
+                owner.phoneNumber.onNext(result)
+            }.disposed(by: disposeBag)
     }
     
     @objc func nextButtonClicked() {
