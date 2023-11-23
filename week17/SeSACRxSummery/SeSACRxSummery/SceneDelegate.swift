b8a1c3655fbc544fd678e54f54ca90883872c635
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-11-23 16:48:07 +0900

    Update SceneDelegate.swift

--- week17/SeSACRxSummery/SeSACRxSummery/SceneDelegate.swift
+++ week17/SeSACRxSummery/SeSACRxSummery/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let vc = BoxOfficeViewController() //Entry Point View Controller
+        let vc = RxDatasourceListViewController() //Entry Point View Controller
+//        let vc = BoxOfficeViewController() //Entry Point View Controller
         window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
     }
