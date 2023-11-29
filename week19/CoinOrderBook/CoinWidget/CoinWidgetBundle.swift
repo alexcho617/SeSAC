3cf914572348ebdbf62e143678f5e9da0b308322
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-11-29 12:00:07 +0900

    SwiftUI: Widget_ Static Config Example

---
+++ week19/CoinOrderBook/CoinWidget/CoinWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CoinWidgetBundle.swift
+//  CoinWidget
+//
+//  Created by Alex Cho on 11/29/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CoinWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CoinWidget()
+    }
+}
