83879d9b9f74ab0610eef1ab1caf8e2e642e4880
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-12-29 14:06:16 +0900

    CoreMLInit

---
+++ CoreMLPractice/CoreMLPractice/CoreMLPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLPracticeApp.swift
+//  CoreMLPractice
+//
+//  Created by Alex Cho on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
